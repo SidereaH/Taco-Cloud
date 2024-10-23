2c48f0838d2636a359d420ab47d50dd759863c45
SidereaH <hutornoyaa@gmail.com>
2024-10-23 22:52:33 +0300

    modify order form, add jakarta validator

--- src/main/java/com/tacos/taco_cloud/Taco.java
+++ src/main/java/com/tacos/taco_cloud/Taco.java
@@ -1,10 +1,17 @@
 package com.tacos.taco_cloud;
 
 import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
+
     private List<Ingredient> ingredients;
 }
